210ecc9d4182e8e1b1f0bf533fdfbe7a9b53a7d1
Berke Algül <47500887+berkealgul@users.noreply.github.com>
2020-09-26 19:01:25 +0300

    Add files via upload

--- rover_21_cv/src/camera_node.cpp
+++ rover_21_cv/src/camera_node.cpp
@@ -1,3 +1,4 @@
+#include <vector>
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <opencv2/opencv.hpp>
@@ -10,20 +11,95 @@ class CameraPub
 public:
   	image_transport::Publisher color_pub, gray_pub;
   	ros::Publisher info_pub;
+<<<<<<< HEAD
+
+  	sensor_msgs::CameraInfo info; //we have fixed camera parameters
+=======
+>>>>>>> d57da7d6ca9079783623660e902043048eed3e61
 
 	bool gray; 
 	std::string name;
 	cv::VideoCapture cap;
 	
-	CameraPub();
+	CameraPub(ros::NodeHandle);
 	void main();
 	void pub_image();
 	void pub_info();
+<<<<<<< HEAD
+	void initialize_info_stuff(ros::NodeHandle);
+	void initialize_image_stuff(ros::NodeHandle);
+=======
+>>>>>>> d57da7d6ca9079783623660e902043048eed3e61
 };
 
-CameraPub::CameraPub()
+CameraPub::CameraPub(ros::NodeHandle nh)
 {
-	ros::NodeHandle nh("~");
+	initialize_info_stuff(nh);
+	initialize_image_stuff(nh);
+
+	ROS_INFO("Initialization completed. we shall begin publishing");
+	ROS_INFO_STREAM("Convert to gray: " << gray);
+
+	//we shall begin publishing
+	main();
+}
+
+void CameraPub::initialize_info_stuff(ros::NodeHandle nh)
+{
+	bool success = true;
+
+	//creating camera info msg
+	std::string distortion_model, info_topic, camera, param_address_header;
+	std::vector<float> D;
+	float fx, fy, cx, cy;
+	int w, h;
+
+	success = nh.getParam("camera", camera);
+
+	param_address_header = "/"+camera+"/";	
+
+	ROS_INFO_STREAM(param_address_header);
+
+	success = nh.getParam("/camera/info_topic", info_topic);
+	success = nh.getParam(param_address_header+"distortion_model", distortion_model);
+	success = nh.getParam(param_address_header+"D", D);
+	success = nh.getParam(param_address_header+"width", w);
+	success = nh.getParam(param_address_header+"height", h);
+	success = nh.getParam(param_address_header+"fx", fx);
+	success = nh.getParam(param_address_header+"fy", fy);
+	success = nh.getParam(param_address_header+"cx", cx);
+	success = nh.getParam(param_address_header+"cy", cy);
+
+	
+	if(!success)
+	{
+		ROS_ERROR("Something went wrong with params. Terminating!!!");
+		ros::shutdown();
+	}
+
+	info.K[0] = fx;
+	info.K[1] = 0;
+	info.K[2] = cx;
+	info.K[3] = 0;
+	info.K[4] = fy;
+	info.K[5] = cy;
+	info.K[6] = 0;
+	info.K[7] = 0;
+	info.K[8] = 1;
+
+
+	for(int i = 0, n = D.size(); i < n; i++)
+		info.D.push_back(D[i]);
+
+	info.width = w;
+	info.height = h;
+	info.distortion_model = distortion_model;
+
+	info_pub = nh.advertise<sensor_msgs::CameraInfo>(info_topic, 1);
+}
+
+void CameraPub::initialize_image_stuff(ros::NodeHandle nh)
+{
 	bool success = true;
 
 	//creating camera info msg
@@ -34,7 +110,6 @@ CameraPub::CameraPub()
 
 	// private params
 	success = nh.getParam("gray", gray);
-	success = nh.getParam("name", name);
 
 	// publisher topics
 	std::string color_topic, gray_topic;
@@ -62,12 +137,15 @@ CameraPub::CameraPub()
 
 	color_pub = it.advertise(color_topic, 1);
 	gray_pub = it.advertise(gray_topic, 1);
+<<<<<<< HEAD
+=======
 
 	ROS_INFO("Initialization completed. we shall begin publishing");
 	ROS_INFO_STREAM("Convert to gray: " << gray);
 
 	//we shall begin publishing
 	main();
+>>>>>>> d57da7d6ca9079783623660e902043048eed3e61
 }
 
 void CameraPub::main()
@@ -103,13 +181,18 @@ void CameraPub::pub_image()
 
 void CameraPub::pub_info()
 {
+<<<<<<< HEAD
+	info_pub.publish(info);
+=======
 
+>>>>>>> d57da7d6ca9079783623660e902043048eed3e61
 }
 
 
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "camera_node");
-	CameraPub();
+	ros::NodeHandle nh("~");
+	CameraPub cp(nh);
 	return 0;
 }
